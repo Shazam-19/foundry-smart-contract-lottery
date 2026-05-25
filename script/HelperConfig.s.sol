c80e7deb4b85cccea6654f2916c59566220c3891
shazam <abdelrahman.sayed2015.p6636@gmail.com>
2026-05-25 23:43:25 +0300

    Added Func getSepoliaEthConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -4,7 +4,15 @@ pragma solidity ^0.8.19;
 import {Script, console} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
 
-contract HelperConfig is Script {
+abstract contract CodeConstants {
+    uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
+    uint256 public constant LOCAL_CHAIN_ID = 31337;
+}
+
+contract HelperConfig is CodeConstants, Script {
+
+    error HelperConfig__InvalidChainId();
+
     struct NetworkConfig {
         uint256 enteranceFee;
         uint256 interval;
@@ -17,6 +25,22 @@ contract HelperConfig is Script {
     NetworkConfig public localNetworkConfig;
     mapping(uint256 chainId => NetworkConfig) public networkConfigs;
 
-    constructor() {}
+    constructor() {
+        networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
+    }
+
+    
+
+    function getSepoliaEthConfig() public pure returns(NetworkConfig memory) {
+
+        return NetworkConfig({
+            enteranceFee: 0.01 ether, // 1e16
+            interval: 30, // 30 seconds
+            vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            subscriptionId: 0,
+            callBackGasLimit: 500000 // 500,000 gas
+        });
+    }
 
 }
