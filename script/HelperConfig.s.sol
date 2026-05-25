0c1bdf3bca35ec6df98062b9b7df741b0260216a
shazam <abdelrahman.sayed2015.p6636@gmail.com>
2026-05-25 23:28:26 +0300

    Initialized HelperConfig File

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Raffle} from "src/Raffle.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        uint256 enteranceFee;
+        uint256 interval;
+        address vrfCoordinator;
+        bytes32 gasLane;
+        uint256 subscriptionId;
+        uint32 callBackGasLimit;
+    }
+
+    NetworkConfig public localNetworkConfig;
+    mapping(uint256 chainId => NetworkConfig) public networkConfigs;
+
+    constructor() {}
+
+}
